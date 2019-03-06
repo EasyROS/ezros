7bf14d725299c36a3c8c243bc5b56537b62c0077
chinadeanls <ls.dean.ch@gmail.com>
2019-03-06 12:43:58 +0800

    Debug

--- manager/setup.py
+++ manager/setup.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.DEBUG,
 def run(arg):
     logging.info('Setup')
     os.system('catkin init --workspace .')
-    os.system('catkin config --profile debug -x _debug --cmake-args -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-msse2"')
+    os.system('catkin config --profile debug -x _debug --cmake-args -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-msse2" -DTest=ON')
     os.system('catkin config --profile release -x _release --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-msse2"')
     os.system('catkin config --profile novision -x _nv_release --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-msse2" -DBUILD_KID_SIZE_VISION=OFF')
     os.system('catkin config --profile novision_debug -x _nv_debug --cmake-args -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-msse2" -DBUILD_KID_SIZE_VISION=OFF')
